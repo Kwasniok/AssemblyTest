83db8f71e8d6940e82fb36aae27976b9fad4fb19
Kwasniok <Kwasniok@users.noreply.github.com>
2019-12-09 16:22:58 +0100

    generalized buils and run script

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-name="main"
+# first argument is the target_name of the target
+target_name=$1
 
 # build
-nasm -f macho src/$name.asm
-ld -macosx_version_min 10.7.0 -o bin/$name src/$name.o
+nasm -f macho src/$target_name.asm
+ld -macosx_version_min 10.7.0 -o bin/$target_name src/$target_name.o
 # run
-./bin/$name
+./bin/$target_name
